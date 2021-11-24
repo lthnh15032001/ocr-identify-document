a10360a55db7dd73f49a1ae34379d7d1b503540d
luongvantuit <luongvantuit@gmail.com>
2021-11-24 09:56:37 +0700

    format static templates

--- Main.py
+++ Main.py
@@ -2,4 +2,4 @@ from providers.Server import Server
 
 
 if __name__ == '__main__':
-    Server().initialaztion()
+    Server.initialization()
--- providers/Server.py
+++ providers/Server.py
@@ -4,12 +4,14 @@ from waitress import serve
 
 class Server:
 
-    _flask: Flask
-
-    def __init__(self):
-        self._flask = Flask(__name__)
+    _flask: Flask = Flask(__name__)
 
     # Initial server flask
     # Run app on 8080
-    def initialization(self):
-        serve(self._flask, port=8080)
+    @staticmethod
+    def initialization():
+        serve(Server._flask, port=8080)
+
+    @staticmethod
+    def instance() -> Flask:
+        return Server._flask
