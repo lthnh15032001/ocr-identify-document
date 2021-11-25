fc140ed26543f4203956a5bcb5b3654f15c83a90
luongvantuit <luongvantuit@gmail.com>
2021-11-25 10:41:04 +0700

    set name flask

--- providers/Server.py
+++ providers/Server.py
@@ -5,7 +5,7 @@ from waitress import serve
 class Server:
 
     _flask: Flask = Flask(
-        __name__, template_folder="../resources/views", static_folder="../resources")
+        "__main__", template_folder="resources/views", static_folder="resources")
 
     # Initial server flask
     # Run app on 8080
@@ -13,3 +13,8 @@ class Server:
     @staticmethod
     def initialization():
         serve(Server._flask, port=8080)
+
+# Comment Test
+# @Server._flask.route('/')
+# def hello():
+#     return render_template('index.html')
