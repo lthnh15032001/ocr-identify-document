ee89075578060a9a7fd1f32d1c5d78117bb7b0a2
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-02 20:30:12 +0700

    404 responsive

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -1,11 +1,2 @@
-echo $OSTYPE
+#!/bin/bash
 
-if [[ $OSTYPE == "Win32" ]]; then 
-    echo "Oh no! Win32 unsupport."
-elif [[ $OSTYPE == "drawin"* ]]; then
-    python3 -m venv ./env
-
-    source env/bin/activate
-
-    pip3 install -r ./requirements.txt
-fi
--- scripts/run.sh
+++
@@ -1,8 +0,0 @@
-echo $OSTYPE
-
-if [[ $OSTYPE == "Win32" ]];
-then 
-    echo "Oh no! Win32 unsupport."
-else 
-    python3 server.py
-fi
