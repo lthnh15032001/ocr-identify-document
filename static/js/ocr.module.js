106fa228ac3b21f7ccefc76023530a5238199776
Lương Văn Tú <luongvantuit@gmail.com>
2022-01-13 19:48:11 +0700

    base page index

--- static/js/ocr.module.js
+++ static/js/ocr.module.js
@@ -1,3 +1,26 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
+
 })
+
+$(document).ready(function () {
+    let preview__file = document.getElementById('preview__file');
+    if (preview__file && preview__file.getAttribute('src') && preview__file.getAttribute('src').length !== 0)
+        $('#preview__file')
+            .show();
+    else
+        $('#preview__file')
+            .hide();
+})
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $('#preview__file')
+                .attr('src', e.target.result);
+            $('#preview__file')
+                .show();
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}
