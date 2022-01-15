1670e165a106f8e6b56f0921aa11767c4c4fc584
Huy <nguyenduchuy_t64@hus.edu.vn>
2022-01-15 21:53:37 +0700

    lại chuyển từ fastapi sang flask

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from app.main import app
-import uvicorn
+# import
+# import uvicorn
 
-uvicorn.run(app, host="localhost", port=8000)
+# uvicorn.run(app, host="localhost", port=8000)
+# load_dotenv()
+app.run(host="localhost", port=8000)
