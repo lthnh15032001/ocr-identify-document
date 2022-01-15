1026d04e121508d41de9091a3ce1058b776ff099
Huy <nguyenduchuy_t64@hus.edu.vn>
2022-01-15 21:23:25 +0700

    chỉ có crop với detect chứ chưa xoay

--- server.py
+++ server.py
@@ -1,37 +1,4 @@
-import os
-from flask import Flask, render_template
-from waitress import serve
-from dotenv import load_dotenv
+from app.main import app
+import uvicorn
 
-# Load configuration environment
-load_dotenv()
-
-# Initialization application
-app: Flask = Flask(__name__)
-
-# Read variable environment port
-port = os.getenv('PORT')
-if port == None:
-    port = 8080
-else:
-    port = int(port)
-
-# Read variable environment
-flask_env = os.getenv('FLASK_ENV')
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    if flask_env == 'dev':
-        app.run(port=port)
-    else:
-        serve(app=app, port=port)
+uvicorn.run(app, host="localhost", port=8000)
